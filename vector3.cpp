131e9942c49c9545b7c19afe17a06a1bfba45034
Luca Cavalleri <luke@localhost.localdomain>
2019-03-30 17:06:48 +0100

    Updated files

--- vector3.cpp
+++ vector3.cpp
@@ -1,66 +1,70 @@
-template<typename D> class vector3
+#include "vector3.h"
+#include <cstdlib>
+#include <cmath>
+
+vector3::vector3() : x(0), y(0), z(0) {}
+vector3::vector3(dtype v) : x(v), y(v), z(v) {}
+vector3::vector3(dtype xp,dtype yp,dtype zp) : x(xp), y(yp), z(zp) {}
+vector3::vector3(const vector3& o) : x(o.x), y(o.y), z(o.z) {}
+
+dtype vector3::magnitude() const {return std::sqrt(x*x+y*y+z*z);}
+vector3 vector3::operator+(const vector3& o) const {return vector3(x+o.x,y+o.y,z+o.z);}
+void vector3::operator+=(const vector3& o){x+=o.x;y+=o.y;z+=o.z;}
+vector3 vector3::operator-(const vector3& o) const {return vector3(x-o.x,y-o.y,z-o.z);}
+void vector3::operator-=(const vector3& o){x-=o.x;y-=o.y;z-=o.z;}
+vector3 vector3::operator*(const vector3& o) const {return vector3(x*o.x,y*o.y,z*o.z);}
+void vector3::operator*=(const vector3& o){x*=o.x;y*=o.y;z*=o.z;}
+vector3 vector3::operator/(const vector3& o) const {return vector3(x/o.x,y/o.y,z/o.z);}
+void vector3::operator/=(const vector3& o){x/=o.x;y/=o.y;z/=o.z;}
+vector3 vector3::operator+(dtype o) const {return vector3(x+o,y+o,z+o);}
+void vector3::operator+=(dtype o){x+=o;y+=o;z+=o;}
+vector3 vector3::operator-(dtype o) const {return vector3(x-o,y-o,z-o);}
+vector3 vector3::operator-() const {return vector3(-x,-y,-z);}
+void vector3::operator-=(dtype o){x-=o;y-=o;z-=o;}
+vector3 vector3::operator*(dtype o) const {return vector3(x*o,y*o,z*o);}
+void vector3::operator*=(dtype o){x*=o;y*=o;z*=o;}
+vector3 vector3::operator/(dtype o) const {return vector3(x/o,y/o,z/o);}
+void vector3::operator/=(dtype o){x/=o;y/=o;z/=o;}
+
+dtype vector3::distance(const vector3& o) const {return std::sqrt((x-o.x)*(x-o.x)+(y-o.y)*(y-o.y)+(z-o.z)*(z-o.z));}
+dtype vector3::distance_sqr(const vector3& o) const {return (x-o.x)*(x-o.x)+(y-o.y)*(y-o.y)+(z-o.z)*(z-o.z);}
+dtype vector3::dot(const vector3& o) const {return x*o.x+y*o.y+z*o.z;}
+vector3 vector3::cross(const vector3& o) const {return vector3(y*o.z-z*o.y,z*o.x-x*o.z,x*o.y-y*o.x);}
+vector3 vector3::power(dtype o) const {return vector3(std::pow(x,o),std::pow(y,o),std::pow(z,o));}
+vector3 vector3::inverse() const {return vector3(1/x,1/y,1/z);}
+vector3 vector3::normalised() const {dtype m=magnitude();return vector3(x/m,y/m,z/m);}
+void vector3::normalise(){dtype m=magnitude();x/=m;y/=m;z/=m;}
+
+vector3 vector3::reflected_in(const vector3& n) const
 {
-public:
-    D x,y,z;
-    vector3(){x=y=z=0;};
-    vector3(D v){x=y=z=v;}
-    vector3(D xp,D yp,D zp){x=xp;y=yp;z=zp;};
-    vector3(D* p){x=*p;y=p[1];z=p[2];}
-    D magnitude() const {return std::sqrt(x*x+y*y+z*z);};
-    
-    vector3 operator+(const vector3& o) const {return vector3(x+o.x,y+o.y,z+o.z);}
-    void operator+=(const vector3& o){x+=o.x;y+=o.y;z+=o.z;}
-    vector3 operator-(const vector3& o) const {return vector3(x-o.x,y-o.y,z-o.z);}
-    void operator-=(const vector3& o){x-=o.x;y-=o.y;z-=o.z;}
-    vector3 operator*(const vector3& o) const {return vector3(x*o.x,y*o.y,z*o.z);}
-    void operator*=(const vector3& o){x*=o.x;y*=o.y;z*=o.z;}
-    vector3 operator/(const vector3& o) const {return vector3(x/o.x,y/o.y,z/o.z);}
-    void operator/=(const vector3& o){x/=o.x;y/=o.y;z/=o.z;}
-    vector3 operator+(D o) const {return vector3(x+o,y+o,z+o);}
-    void operator+=(D o){x+=o;y+=o;z+=o;}
-    vector3 operator-(D o) const {return vector3(x-o,y-o,z-o);}
-    vector3 operator-() const {return vector3(-x,-y,-z);}
-    void operator-=(D o){x-=o;y-=o;z-=o;}
-    vector3 operator*(D o) const {return vector3(x*o,y*o,z*o);}
-    void operator*=(D o){x*=o;y*=o;z*=o;}
-    vector3 operator/(D o) const {return vector3(x/o,y/o,z/o);}
-    void operator/=(D o){x/=o;y/=o;z/=o;}
-    
-    D distance(const vector3& o) const {return std::sqrt((x-o.x)*(x-o.x)+(y-o.y)*(y-o.y)+(z-o.z)*(z-o.z));}
-    D distance_sqr(const vector3& o) const {return (x-o.x)*(x-o.x)+(y-o.y)*(y-o.y)+(z-o.z)*(z-o.z);}
-    D dot(const vector3& o) const {return x*o.x+y*o.y+z*o.z;}
-    vector3 cross(const vector3& o) const {return vector3(y*o.z-z*o.y,z*o.x-x*o.z,x*o.y-y*o.x);}
-    vector3 power(D o) const {return vector3(std::pow(x,o),std::pow(y,o),std::pow(z,o));}
-    vector3 inverse() const {return vector3(1/x,1/y,1/z);}
-    vector3 normalised() const {D m=magnitude();return vector3(x/m,y/m,z/m);}
-    void normalise(){D m=magnitude();x/=m;y/=m;z/=m;}
-    vector3 reflected_in(const vector3& n) const
-    {
-        D d = dot(n)*2;
-        return vector3(d*n.x-x,d*n.y-y,d*n.z-z);
-    }
-    static vector3 random_inside_unit_sphere()
-    {
-        D r = ((D) std::rand() / (RAND_MAX));
-        D theta = ((D) std::rand() / (RAND_MAX)) * M_PIl;
-        D phi = ((D) std::rand() / (RAND_MAX)) * M_PIl * 2;
-        return vector3(r*std::sin(theta)*std::cos(phi),
-                       r*std::sin(theta)*std::sin(phi),
-                       r*std::cos(theta));
-    }
-    static D random_01()
-    {
-        return ((D) std::rand() / (RAND_MAX));
-    }
-    bool operator==(const vector3& o) const {return x==o.x && y==o.y && z==o.z;}
-    friend std::ostream& operator<<(std::ostream& os, const vector3& v)  
-    {  
-        os << '[' << v.x << ',' << v.y << ',' << v.z << ']';  
-        return os;  
-    }
-    vector3& operator=(const vector3& o)
-    {
-        x=o.x;y=o.y;z=o.z;
-        return *this;
-    }
-};
+    dtype d = dot(n)*2;
+    return vector3(d*n.x-x,d*n.y-y,d*n.z-z);
+}
+vector3 vector3::random_inside_unit_sphere()
+{
+    dtype r = ((dtype) std::rand() / (RAND_MAX));
+    dtype theta = ((dtype) std::rand() / (RAND_MAX)) * M_PIl;
+    dtype phi = ((dtype) std::rand() / (RAND_MAX)) * M_PIl * 2;
+    return vector3(r*std::sin(theta)*std::cos(phi),
+                    r*std::sin(theta)*std::sin(phi),
+                    r*std::cos(theta));
+}
+dtype vector3::random_01()
+{
+    return ((dtype) std::rand() / (RAND_MAX));
+}
+
+bool vector3::operator==(const vector3& o) const {return x==o.x && y==o.y && z==o.z;}
+bool vector3::operator!=(const vector3& o) const {return x!=o.x || y!=o.y || z!=o.z;}
+
+std::ostream& operator<< (std::ostream& os, const vector3& v)
+{  
+    os << '[' << v.x << ',' << v.y << ',' << v.z << ']';  
+    return os;  
+}
+
+vector3& vector3::operator=(const vector3& o)
+{
+    x=o.x;y=o.y;z=o.z;
+    return *this;
+}
--- vector3.h
+++ vector3.h
@@ -1,16 +1,19 @@
 #ifndef _INCLUDE_VECTOR3_H_
 #define _INCLUDE_VECTOR3_H_
 
+#include "gfx.h"
+#include <iostream>
+
 class vector3
 {
-    typedef floating_point_type D;
+    
 public:
-    D x,y,z;
+    dtype x,y,z;
     vector3();
-    vector3(D v);
-    vector3(D xp,D yp,D zp);
-    vector3(D* p);
-    D magnitude() const;
+    vector3(dtype v);
+    vector3(dtype xp,dtype yp,dtype zp);
+    vector3(const vector3& o);
+    dtype magnitude() const;
     
     vector3 operator+(const vector3& o) const ;
     void operator+=(const vector3& o);
@@ -20,29 +23,29 @@ public:
     void operator*=(const vector3& o);
     vector3 operator/(const vector3& o) const ;
     void operator/=(const vector3& o);
-    vector3 operator+(D o) const ;
-    void operator+=(D o){x+=o;y+=o;z+=o;}
-    vector3 operator-(D o) const ;
+    vector3 operator+(dtype o) const ;
+    void operator+=(dtype o);
+    vector3 operator-(dtype o) const ;
     vector3 operator-() const ;
-    void operator-=(D o);
-    vector3 operator*(D o) const ;
-    void operator*=(D o);
-    vector3 operator/(D o) const ;
-    void operator/=(D o);
+    void operator-=(dtype o);
+    vector3 operator*(dtype o) const ;
+    void operator*=(dtype o);
+    vector3 operator/(dtype o) const ;
+    void operator/=(dtype o);
     
-    D distance(const vector3& o) const ;
-    D distance_sqr(const vector3& o) const ;
-    D dot(const vector3& o) const ;
+    dtype distance(const vector3& o) const ;
+    dtype distance_sqr(const vector3& o) const ;
+    dtype dot(const vector3& o) const ;
     vector3 cross(const vector3& o) const ;
-    vector3 power(D o) const ;
+    vector3 power(dtype o) const ;
     vector3 inverse() const ;
     vector3 normalised() const ;
     void normalise();
-    vector3 reflected_in(const vector3& n);
+    vector3 reflected_in(const vector3& n) const;
     static vector3 random_inside_unit_sphere();
-    static D random_01();
-    bool operator==(const vector3& o);
-    bool operator!=(const vector3& o);
+    static dtype random_01();
+    bool operator==(const vector3& o) const;
+    bool operator!=(const vector3& o) const;
     friend std::ostream& operator<<(std::ostream& os, const vector3& v);
     vector3& operator=(const vector3& o);
 };
